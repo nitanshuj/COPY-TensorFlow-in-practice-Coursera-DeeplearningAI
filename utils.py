e38dd618d425e2b4fd5db59c5d867739f201453c
nitanshuj <nitanshuj138@gmail.com>
2023-07-02 17:35:13 -0400

    Updated Utils

--- utils.py
+++ utils.py
@@ -88,3 +88,13 @@ def noise(time, noise_level=1, seed=None):
     noise = rnd.randn(len(time)) * noise_level    
     return noise
 # =============================================================================================
+
+def generate_synthetic_timeseries(baseline=10, amplitude=40, slope=0.05, noise_level=5):
+    time = np.arange(4 * 365 + 1, dtype="float32")
+    # Create the series
+    series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+    # Update with noise
+    series += noise(time, noise_level, seed=42)
+    return series, time
+
+# =============================================================================================
