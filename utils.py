03537a8243b4146db8e247f436542fefc21707ef
nitanshuj <nitanshuj138@gmail.com>
2023-07-04 13:51:30 -0400

    Course 4 Week 3 - Initial Commit

--- utils.py
+++ utils.py
@@ -98,3 +98,67 @@ def generate_synthetic_timeseries(baseline=10, amplitude=40, slope=0.05, noise_l
     return series, time
 
 # =============================================================================================
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    """Generates dataset windows
+
+    Args:
+      series (array of float) - contains the values of the time series
+      window_size (int) - the number of time steps to include in the feature
+      batch_size (int) - the batch size
+      shuffle_buffer(int) - buffer size to use for the shuffle method
+
+    Returns:
+      dataset (TF Dataset) - TF Dataset containing time windows
+    """
+  
+    # Generate a TF Dataset from the series values
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    
+    # Window the data but only take those with the specified size
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    
+    # Flatten the windows by putting its elements in a single batch
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+
+    # Create tuples with features and labels 
+    dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+
+    # Shuffle the windows
+    dataset = dataset.shuffle(shuffle_buffer)
+    
+    # Create batches of windows
+    dataset = dataset.batch(batch_size).prefetch(1)
+    
+    return dataset
+
+## ---------------------------------------------------------------------------------
+
+def model_forecast(model, series, window_size, batch_size):
+    """Uses an input model to generate predictions on data windows
+
+    Args:
+      model (TF Keras Model) - model that accepts data windows
+      series (array of float) - contains the values of the time series
+      window_size (int) - the number of time steps to include in the window
+      batch_size (int) - the batch size
+
+    Returns:
+      forecast (numpy array) - array containing predictions
+    """
+
+    # Generate a TF Dataset from the series values
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+
+    # Window the data but only take those with the specified size
+    dataset = dataset.window(window_size, shift=1, drop_remainder=True)
+
+    # Flatten the windows by putting its elements in a single batch
+    dataset = dataset.flat_map(lambda w: w.batch(window_size))
+    
+    # Create batches of windows
+    dataset = dataset.batch(batch_size).prefetch(1)
+    
+    # Get predictions on the entire dataset
+    forecast = model.predict(dataset)
+    
+    return forecast
